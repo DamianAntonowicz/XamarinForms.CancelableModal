d1e5c5010f24523e4ff2048e9e588a5c987f7f7b
Damian Antonowicz <kontakt@damianantonowicz.pl>
2018-06-23 16:39:30 +0200

    Initial commit with blank Forms app.

---
+++ XamarinForms.CancelableModalPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinForms.CancelableModal
+{
+    public partial class XamarinForms_CancelableModalPage : ContentPage
+    {
+        public XamarinForms_CancelableModalPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
