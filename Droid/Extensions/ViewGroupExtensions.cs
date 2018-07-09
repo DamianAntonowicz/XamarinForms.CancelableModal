7467c96a3871dc471c9f0fe33458b62e71d01cd3
Damian Antonowicz <kontakt@damianantonowicz.pl>
2018-07-10 00:16:40 +0200

    Code refactor.

--- Droid/Extensions/ViewGroupExtensions.cs
+++ Droid/Extensions/ViewGroupExtensions.cs
@@ -4,41 +4,58 @@ namespace Android.Views
 {
     public static class ViewGroupExtensions
     {
+        public static IEnumerable<T> GetChildrenOfType<T>(this ViewGroup viewGroup)
+            where T : View
+        {
+            for (int i = 0; i < viewGroup.ChildCount; i++)
+            {
+                View child = viewGroup.GetChildAt(i);
+
+                if (child is T expectedChild)
+                {
+                    yield return expectedChild;
+                }
+
+                if (child is ViewGroup childViewGroup)
+                {
+                    foreach (var expectedInnerChild in GetChildrenOfType<T>(childViewGroup))
+                    {
+                        yield return expectedInnerChild;
+                    }
+                }
+            }
+        }
+
+#if DEBUG
         private const string PADDING_STRING = "  ";
         private static string _currentPadding = "";
-
-        public static IList<T> GetChildrenOfType<T>(this ViewGroup viewGroup)
-            where T : class
+        
+        /// <summary>
+        /// Helper method for printing on output all children from ViewGroup.
+        /// </summary>
+        public static void PrintChildren(this ViewGroup viewGroup)
         {
-            var listToReturn = new List<T>();
-
             System.Diagnostics.Debug.WriteLine(_currentPadding + viewGroup.GetType().Name + " (ViewGroup)");
             
             _currentPadding += PADDING_STRING;
 
             for (int i = 0; i < viewGroup.ChildCount; i++)
             {
-                Android.Views.View child = viewGroup.GetChildAt(i);
+                View child = viewGroup.GetChildAt(i);
                 
                 if (!(child is ViewGroup))
                 {
                     System.Diagnostics.Debug.WriteLine(_currentPadding + child.GetType().Name);
                 }
 
-                if (child.GetType() == typeof(T))
-                {
-                    listToReturn.Add(child as T);
-                }
-
                 if (child is ViewGroup childViewGroup)
                 {
-                    listToReturn.AddRange(GetChildrenOfType<T>(childViewGroup));
+                    PrintChildren(childViewGroup);
                 }
             }
 
             _currentPadding = _currentPadding.Remove(_currentPadding.Length - PADDING_STRING.Length);
-
-            return listToReturn;
         }
+    #endif
     }
 }
